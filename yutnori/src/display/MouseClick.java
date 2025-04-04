c6405372bc29d16989ccbb96c6c39e72e39304d0
hyenwon <vn72045470@gmail.com>
2025-04-04 19:26:34 +0900

    swing -> display 수정

--- yutnori/src/display/MouseClick.java
+++ yutnori/src/display/MouseClick.java
@@ -1,4 +1,4 @@
-package swing;
+package display;
 
 public class MouseClick {
     //마우스 클릭 이벤트를 처리하여 mal을 선택 및 이동시키는 class
--- yutnori/src/display/UiClick.java
+++ yutnori/src/display/UiClick.java
@@ -1,4 +1,4 @@
-package swing;
+package display;
 
 public class UiClick {
     //마우스 클릭 이벤트를 받아서 MouseClick class의 메서드를 호출하는 class
