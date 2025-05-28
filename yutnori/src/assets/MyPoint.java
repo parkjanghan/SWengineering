9c57ce37f5d755f49c90fa0450b84382740fb659
hyenwon <vn72045470@gmail.com>
2025-05-28 21:00:05 +0900

    display

--- yutnori/src/assets/MyPoint.java
+++ yutnori/src/assets/MyPoint.java
@@ -1,10 +1,14 @@
 package assets;
 
 public class MyPoint {
-    public int x, y;
+    public int x;
+    public int y;
 
     public MyPoint(int x, int y) {
         this.x = x;
         this.y = y;
     }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
 }
